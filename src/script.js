9b4cfe2fc97e72a912109b118999f2a9c3c9839b
MellowPhi <ombt14@gmail.com>
2023-07-20 16:02:20 +1000

    feature button added

---
+++ src/script.js
@@ -0,0 +1,21 @@
+// Function to scroll smoothly to the end of the page when the button is clicked
+function scrollToPageEnd() {
+    //const distanceContainer = document.querySelector('.distance-container');
+    const distanceContainer = document.getElementById('bottom');
+    if (distanceContainer) {
+      distanceContainer.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  
+  // Function to initialize the scroll-to-end button
+  function initScrollToEndButton() {
+    // Add an event listener to the scroll-to-end button
+    const scrollToButton = document.getElementById('join-button');
+    if (scrollToButton) {
+      scrollToButton.addEventListener('click', scrollToPageEnd);
+    }
+  }
+  
+  // Add an event listener to handle the scroll-to-end button
+document.addEventListener('DOMContentLoaded', initScrollToEndButton);
+  
